4d542aba0938ee7013152f892028dba831029779
Yousef Meska <mrbomberboy123@gmail.com>
2021-12-17 11:40:07 +0200

    add cart page & auth pages

--- server.js
+++ server.js
@@ -5,12 +5,22 @@ const path = require("path");
 const expressLayout = require("express-ejs-layouts");
 app.use(express.static("public"));
 const viewsPath = path.join(__dirname, "/resources/views");
-//app.use(expressLayout);
+app.use(expressLayout);
 app.set("views", viewsPath);
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+app.get("/register", (req, res) => {
+  res.render("auth/register");
 });
 // load routes
 
