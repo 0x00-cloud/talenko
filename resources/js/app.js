4948b52796731d9750534d804e44d42659f17738
Yousef Meska <mrbomberboy123@gmail.com>
2021-12-19 22:57:46 +0200

    place orders, admin functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Noty from "noty";
+import { initAdmin } from "./admin";
 function updateCart(pizza) {
   axios
     .post("/update-cart", pizza)
@@ -25,6 +26,12 @@ function updateCart(pizza) {
 document.addEventListener("DOMContentLoaded", (e) => {
   const addToCart = document.querySelectorAll(".add-to-cart");
   let cartCounter = document.querySelector("#cartCounter");
+  const alertMsg = document.querySelector("#success-alert");
+  if (alertMsg) {
+    setTimeout(() => {
+      alertMsg.remove();
+    }, 2000);
+  }
   addToCart.forEach((addtoBtn) => {
     addtoBtn.addEventListener("click", function (e) {
       let pizza = JSON.parse(this.dataset.pizza);
@@ -32,3 +39,5 @@ document.addEventListener("DOMContentLoaded", (e) => {
     });
   });
 });
+
+initAdmin();
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,7 +1,11 @@
 const homeController = require("../controllers/homeController");
 const authController = require("../controllers/auth/authController");
 const cartController = require("../controllers/customer/cartController");
+const orderController = require("../controllers/customer/orderController");
+const AdminOrderController = require("../controllers/admin/orderController");
 const guest = require("../middlewares/guest");
+const auth = require("../middlewares/auth");
+const admin = require("../middlewares/admin");
 function initRoutes(app) {
   app.get("/", homeController.index);
 
@@ -15,6 +19,14 @@ function initRoutes(app) {
   app.get("/register", guest, authController.register);
   app.post("/register", authController.postRegister);
   app.post("/logout", authController.logout);
+
+  // Customer
+  app.post("/orders", auth, orderController.store);
+
+  //Customer
+  app.get("/customers/orders", auth, orderController.index);
+
+  app.get("/admin/orders", admin, AdminOrderController.index);
 }
 
 module.exports = initRoutes;
